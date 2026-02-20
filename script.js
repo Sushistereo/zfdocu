2fc1e74360edd1cfa091db6d0b97a7d07e64ed63
fran <gengisfrhan@gmail.com>
2026-02-20 15:38:59 -0300

    new ag

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ document.querySelector('.left-text').addEventListener('click', function() {
     this.classList.add('hidden');
     document.getElementById('left-menu').classList.add('active');
 });
+document.querySelector('.left-text').addEventListener('keypress', function(e) {
+    if(e.key === 'Enter') {
+        this.classList.add('hidden');
+        document.getElementById('left-menu').classList.add('active');
+    }
+});
 
 document.querySelector('.right-text').addEventListener('click', function() {
     this.classList.add('hidden');
     document.getElementById('right-menu').classList.add('active');
+});
+document.querySelector('.right-text').addEventListener('keypress', function(e) {
+    if(e.key === 'Enter') {
+        this.classList.add('hidden');
+        document.getElementById('right-menu').classList.add('active');
+    }
 });
 
 // Código del desplegable de cine eliminado - ahora Cine va directo a cine.html
